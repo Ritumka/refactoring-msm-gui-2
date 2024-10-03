f99195b8c30e5cd3850db633aa2ddb82936689e9
Margarita Potylitsyna <148562840+Ritumka@users.noreply.github.com>
2024-10-03 21:49:58 +0000

    almost

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,7 +2,7 @@
 #
 # Table name: characters
 #
-#  id         :integer          not null, primary key
+#  id         :bigint           not null, primary key
 #  name       :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
@@ -10,13 +10,15 @@
 #  movie_id   :integer
 #
 class Character < ApplicationRecord
-  def movie
-    key = self.movie_id
+  belongs_to(:movie, class_name: "Movie", foreign_key: "movie_id")
 
-    matching_set = Movie.where({ :id => key })
+  # def movie
+  #   key = self.movie_id
 
-    the_one = matching_set.at(0)
+  #   matching_set = Movie.where({ :id => key })
 
-    return the_one
-  end
+  #   the_one = matching_set.at(0)
+
+  #   return the_one
+  # end
 end
